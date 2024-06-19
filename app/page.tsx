c7af12315f05f050435f4744245442b2b53852d6
Kasun Rathnayaka <k.rathnayaka365@gmail.com>
2024-06-19 12:24:25 +0530

    loading screen/ audio file to be added

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,10 @@ export default function Home() {
     <main className=" bg-[#18181B] to-black-100 relative w-full text-white flex flex-col">
       <div><Nav/></div>
       <section><Hero/></section>
+      <section>
+        {/* add a audio file for this section to auto play when the page is loaded */}
+        <audio src="/assets/audi.mp3" autoPlay loop></audio>
+      </section>
       <footer><Footer/></footer>
     </main>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
       {/* create a center footer with center text and top white line  */}
       <div className="flex flex-col gap-2 justify-center items-center h-16 bg-black text-white-200 w-[850px] border-t-[1px] border-white border-opacity-20 text-sm md:w-full">
-        <p>Proudly Open sourced in <Link href={'/'} className='font-bold'>github</Link></p>
+        <p>Proudly Open sourced in <Link href={'https://github.com/kasun-m-rathnayaka/Department-of-IT-Torana'} className='font-bold'>github</Link></p>
         <p>© 2024 All rights reserved</p>
       </div>
     </div>
